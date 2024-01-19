8df923ed4bf8e6d06e1ee9ad2260eb96ae791361
pedroargioni <pedroargioni@gmail.com>
2024-01-19 14:24:57 -0300

    configurado formulario para cadastro de novos flashcards

--- flashcard/views.py
+++ flashcard/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render, redirect
+from .models import Categoria, Flashcard
+from django.contrib.messages import constants
+from django.contrib import messages
 
 
 def novo_flashcard(request):
     if not request.user.is_authenticated:
         return redirect('/usuarios/logar')
+
     if request.method == 'GET':
-        return render(request, 'novo_flashcard.html')
+        categorias = Categoria.objects.all()
+        dificuldades = Flashcard.DIFICULDADE_CHOICES
+        return render(request, 'novo_flashcard.html', {'categorias': categorias, 'dificuldades': dificuldades})
+
+    elif request.method == 'POST':
+        pergunta = request.POST.get('pergunta')
+        resposta = request.POST.get('resposta')
+        categoria = request.POST.get('categoria')
+        dificuldade = request.POST.get('dificuldade')
+
+        if len(pergunta.strip()) == 0 or len(resposta.strip()) == 0:
+            messages.add_message(
+                request, constants.ERROR, 'Preencha corretamente os campos Pergunta e Resposta!')
+            return redirect('/flashcard/novo_flashcard/')
+
+        flashcard = Flashcard(
+            user=request.user,
+            pergunta=pergunta,
+            resposta=resposta,
+            categoria_id=categoria,
+            dificuldade=dificuldade,
+        )
+
+        flashcard.save()
+
+        messages.add_message(request, constants.SUCCESS,
+                             'Flashcard adicionado com sucesso!')
+        return redirect('/flashcard/novo_flashcard/')
